22ac31061c8531809cd199ae40c293a9e1fe4cf3
Randytia Akbar <randytia.akbar@gmail.com>
2023-11-29 11:39:39 +0700

    deploy

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/abarobotics/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/abarobotics/Dart Frog Projects/randy_portofolio"
+export "FLUTTER_APPLICATION_PATH=/Users/abarobotics/IdeaProjects/randy_portofolio"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
