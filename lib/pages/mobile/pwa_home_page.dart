7ea51ed1f4191e28821f17ed05710d05e416ede1
Randytia Akbar <randytia.akbar@gmail.com>
2024-02-25 13:19:18 +0700

    test linmas

--- lib/pages/mobile/pwa_home_page.dart
+++ lib/pages/mobile/pwa_home_page.dart
@@ -1,9 +1,14 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:randy_portofolio/theme_manager/space_manager.dart';
 import 'package:randy_portofolio/utils/launch_url.dart';
 import 'package:responsive_builder/responsive_builder.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:universal_html/html.dart';
 
 import '../../theme_manager/color_manager.dart';
 import '../../widgets/description.dart';
@@ -12,12 +17,33 @@ import '../../widgets/mobile_body.dart';
 import '../../widgets/web_body.dart';
 import '../../widgets/work_experience.dart';
 
-class PwaHomePage extends StatelessWidget {
+class PwaHomePage extends StatefulWidget {
   const PwaHomePage({super.key});
+
+  @override
+  State<PwaHomePage> createState() => _PwaHomePageState();
+}
+
+class _PwaHomePageState extends State<PwaHomePage> {
+  late pw.Document pdf;
+
+  @override
+  void initState() {
+    super.initState();
+    pdf = pw.Document();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          writePdf();
+
+          await savePdf();
+        },
+        child: const Icon(Icons.save),
+      ),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
@@ -108,4 +134,51 @@ class PwaHomePage extends StatelessWidget {
       ),
     );
   }
+
+  Future<void> savePdf() async {
+    final pdfInByte = await pdf.save();
+
+    List<int> fileInts = List.from(pdfInByte);
+    AnchorElement(
+        href:
+            "data:application/octet-stream;charset=utf-16le;base64,${base64.encode(fileInts)}")
+      ..setAttribute("download", "${DateTime.now().millisecondsSinceEpoch}.pdf")
+      ..click();
+  }
+
+  void writePdf() => pdf.addPage(
+        pw.MultiPage(
+          pageFormat: PdfPageFormat.a4,
+          margin: const pw.EdgeInsets.only(
+            left: 32,
+            right: 32,
+            top: 40,
+            bottom: 32,
+          ),
+          build: (_) => [
+            pw.Header(level: 0, child: pw.Text('Easy Approach Document')),
+            pw.Paragraph(
+              text:
+                  'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+            ),
+            pw.Paragraph(
+              text:
+                  'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+            ),
+            pw.Header(level: 1, child: pw.Text('Second Heading')),
+            pw.Paragraph(
+              text:
+                  'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+            ),
+            pw.Paragraph(
+              text:
+                  'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+            ),
+            pw.Paragraph(
+              text:
+                  'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+            )
+          ],
+        ),
+      );
 }
